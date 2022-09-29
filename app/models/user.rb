3e050602524cb1cc8f0d0d344e41917dd465297e
Hai Tran <harefx.dev@gmail.com>
2022-09-30 04:35:47 +0700

    Config jwt secret and algo with dotenv

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :trackable,
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable,
+         :trackable, :jwt_authenticatable, jwt_revocation_strategy: self
 end
